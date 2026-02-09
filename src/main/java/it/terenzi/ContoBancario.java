aab40475a514c05c8485236aefd15b0fa2f1ba2a
le-van-dowski <aksperdelepw@gmail.com>
2026-02-09 11:38:34 +0100

    documentazione

--- src/main/java/it/terenzi/ContoBancario.java
+++ src/main/java/it/terenzi/ContoBancario.java
@@ -1,19 +1,60 @@
 package it.terenzi;
 
+/**    
+ * Classe che rappresenta un conto bancario.
+ * <br>
+ */
 public class ContoBancario {
 
+    /** 
+     * L'IBAN del conto bancario.
+     */
     protected String iban;
+    /** 
+     * Il saldo del conto bancario.
+     */
     protected double saldo;
 
+    /**
+     * Costruttore che inizializza il conto bancario con un IBAN e un saldo.
+     * @param iban il numero del conto bancario
+     * @param saldo il saldo iniziale del conto bancario
+     */
     public ContoBancario(String iban, double saldo) {
         this.iban = iban;
         this.saldo = saldo;
     }
 
+   /**
+     * Restituisce il numero del conto bancario.
+     * @return il numero del conto bancario
+     */
+    public String getIban() {
+        return iban;
+    }
+    /**
+     * Restituisce il saldo del conto bancario.
+     * @return il saldo del conto bancario
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    
+    /**
+     * Deposita una somma di denaro sul conto bancario, incrementando il saldo
+     * @param importo la somma di denaro da depositare
+     * @throws IllegalArgumentException se l'importo è negativo
+     */
     public void deposita(double importo) {
         this.saldo += importo;
     }
 
+    /**
+     * Preleva una somma di denaro dal conto bancario, decrementando il saldo.
+     * @param importo  la somma di denaro da prelevare
+     * @throws IllegalArgumentException se l'importo è negativo o supera il saldo
+     */
     public void preleva(double importo) {
         if (importo > saldo) {
             System.out.println("Fondi insufficienti");
@@ -22,14 +63,19 @@ public class ContoBancario {
         }
     }
 
-    public String getIban() {
-        return iban;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
+    /** 
+     * Restituisce una rappresentazione stringa del conto bancario.
+     * Restituisce una rappresentazione stringa del conto bancario. Include l'IBAN e il saldo. La rappresentazione stringa e' formattata per essere facile da leggere e comprendere. Ecco un esempio di come potrebbe apparire la rappresentazione stringa:
+     * <br>
+     * ```
+     * Conto Bancario:<br>
+     * Saldo: 1000.0 EUR
+     * IBAN: IT12X0542811101000000123
+     * ```
+     * <br>
+     * 
+     * @return una rappresentazione stringa del conto bancario, inclusivo di IBAN e saldo
+     */
     @Override
     public String toString() {
         return "ContoBancario [iban=" + iban + ", saldo=" + saldo + "]";
--- src/main/java/it/terenzi/Main.java
+++ src/main/java/it/terenzi/Main.java
@@ -2,6 +2,13 @@ package it.terenzi;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ContoBancario conto1 = new ContoBancario("IT1234567890", 1000.0);
+        System.out.println(conto1);
+        conto1.deposita(500.0);
+        System.out.println(conto1);
+        conto1.preleva(200.0);
+        System.out.println(conto1);
+        conto1.preleva(1500.0);
+        System.out.println(conto1);
     }
 }
